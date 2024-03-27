97b3d17a3fcd0c5910626a058ca5db83d2ca440e
theanhnguyen <theanhmgt66@gmail.com>
2024-03-27 15:17:51 +0700

    chore: add pring-orm, hibernate, mysql-conntector dependency, build MVC architecture

--- app/src/main/java/com/nta/controllers/IndexController.java
+++ app/src/main/java/com/nta/controllers/IndexController.java
@@ -4,7 +4,10 @@
  */
 package com.nta.controllers;
 
+import com.nta.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -13,8 +16,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class IndexController {
+    @Autowired
+    private CategoryService categoryService;
     @RequestMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("categories",this.categoryService.getCate());
         return "index";
     }
 }
---
+++ app/src/main/java/com/nta/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.nta.service.impl;
+
+import com.nta.pojo.Category;
+import com.nta.repository.CategoryRepository;
+import com.nta.service.CategoryService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author admin
+ */
+@Service
+public class CategoryServiceImpl implements CategoryService{
+    @Autowired
+    private CategoryRepository cateRepo;
+    @Override
+    public List<Category> getCate() {
+        return this.cateRepo.getCate();
+    }
+    
+}
