3d39ab68e6082bd4b5102af0fdbcde393417113d
theanhnguyen <theanhmgt66@gmail.com>
2024-03-27 16:10:50 +0700

    feat: initially implement product pojo in mvc architecture

--- app/src/main/java/com/nta/controllers/IndexController.java
+++ app/src/main/java/com/nta/controllers/IndexController.java
@@ -5,10 +5,13 @@
 package com.nta.controllers;
 
 import com.nta.service.CategoryService;
+import com.nta.service.ProductService;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  *
@@ -18,9 +21,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
     @Autowired
     private CategoryService categoryService;
+    
+    @Autowired
+    private ProductService prodService;
     @RequestMapping("/")
-    public String index(Model model) {
+    public String index(Model model,@RequestParam Map<String, String> params) {
         model.addAttribute("categories",this.categoryService.getCate());
+        model.addAttribute("products", this.prodService.getProducts(params));
         return "index";
     }
 }
---
+++ app/src/main/java/com/nta/service/impl/ProductServiceImpl.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.nta.service.impl;
+
+import com.nta.pojo.Product;
+import com.nta.repository.ProductRepository;
+import com.nta.service.ProductService;
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author admin
+ */
+@Service
+public class ProductServiceImpl implements ProductService{
+    @Autowired
+    private ProductRepository prodRepo;
+
+    @Override
+    public List<Product> getProducts(Map<String, String> params) {
+        return this.prodRepo.getProducts(params);
+    }
+
+    @Override
+    public void addOrUpdate(Product p) {
+        this.prodRepo.addOrUpdate(p);
+    }
+}
